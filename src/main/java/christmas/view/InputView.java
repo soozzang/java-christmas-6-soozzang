37c5995c18f44e7097f2b3373a4ed0365a3042b9
soozzang <soozzang@inha.edu>
2023-11-10 23:15:35 +0900

    feat(InputView): 문자열파싱 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,6 +2,8 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
@@ -9,10 +11,16 @@ public class InputView {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 
     public static List<String> readMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String input = Console.readLine();
+        return stringParser(Console.readLine());
+    }
+
+    public static List<String> stringParser(String menuInput) {
+        String[] parsedMenuStr = menuInput.split(",");
+        return new ArrayList<>(Arrays.asList(parsedMenuStr));
     }
 }
