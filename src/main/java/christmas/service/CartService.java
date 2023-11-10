6d9b13e6698be2ffcb9b1c4b3483962648a85113
soozzang <soozzang@inha.edu>
2023-11-11 00:37:22 +0900

    feat(CartService): 예외처리 완료

--- src/main/java/christmas/service/CartService.java
+++ src/main/java/christmas/service/CartService.java
@@ -13,13 +13,19 @@ public class CartService {
 
     public static Cart makeCart() {
         Cart cart = new Cart();
-        List<String> menuInput = InputView.readMenu();
-        HashMap<String, Integer> menuMap = makeMenuMap(menuInput);
+        while (true) {
+            try {
+                List<String> menuInput = InputView.readMenu();
+                HashMap<String, Integer> menuMap = makeMenuMap(menuInput);
 
-        for (String menuName : menuMap.keySet()) {
-            Menu menu = Menu.getMenu(menuName);
-            cart.addMenuInCart(menu, menuMap.get(menuName));
+                for (String menuName : menuMap.keySet()) {
+                    Menu menu = Menu.getMenu(menuName);
+                    cart.addMenuInCart(menu, menuMap.get(menuName));
+                }
+                return cart;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
-        return cart;
     }
 }
