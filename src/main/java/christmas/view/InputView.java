40db3f2873f2b723354fe52b99762183faffd732
soozzang <soozzang@inha.edu>
2023-11-11 00:37:14 +0900

    feat(InputView): 예외처리 완료

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,17 +1,44 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Menu;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class InputView {
     public static int readDate() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
-        return Integer.parseInt(input);
+        while (true) {
+            try {
+                int date = validateDateIsInt(Console.readLine());
+                dateValidations(date);
+                return date;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void dateValidations(int date) {
+        validateDateIsOutOfRange(date);
+    }
+
+    public static int validateDateIsInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public static void validateDateIsOutOfRange(int date) {
+        if (!(date >= 1 && date <= 31)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
     }
 
     public static List<String> readMenu() {
@@ -22,5 +49,37 @@ public class InputView {
     public static List<String> stringParser(String menuInput) {
         String[] parsedMenuStr = menuInput.split(",");
         return new ArrayList<>(Arrays.asList(parsedMenuStr));
+    }
+
+    public static void validateCommaAfterComma(String menuInput) {
+
+    }
+
+    public static void validateIsContainBlank(String menuInput) {
+
+    }
+
+    public static void validateLastCharIsComma(String menuInput) {
+
+    }
+
+    public static void validateIsNotInMenu(String menuName) {
+        boolean flag = false;
+        for (Menu menu : Menu.values()) {
+            if (menu.getKorName().equals(menuName)) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public static void validateDuplicateMenu(List<String> nameList) {
+        HashSet<String> menuSet = new HashSet<>(nameList);
+        if (menuSet.size() != nameList.size()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 }
