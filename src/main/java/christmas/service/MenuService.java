2dab48540cebac127473945e8ab3d642bff798c0
soozzang <soozzang@inha.edu>
2023-11-10 14:59:46 +0900

    feat(MenuService): MenuService, 컨트롤러 기본 뼈대 설계

---
+++ src/main/java/christmas/service/MenuService.java
@@ -0,0 +1,15 @@
+package christmas.service;
+
+import christmas.domain.Menu;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class MenuService {
+
+    // 입력에서 List<String>으로 받은 걸 Menu 맵으로 바꿈
+    // 콤마 기준 잘라서 문자 넣고 숫자 넣어서 완성
+    public static HashMap<String, Integer> makeMenuMap(List<String> menuInput) {
+
+    }
+}
