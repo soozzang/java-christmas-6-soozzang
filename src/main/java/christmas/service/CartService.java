753e0b2fce759a6f108602cad3f5b63976afc86b
soozzang <soozzang@inha.edu>
2023-11-10 23:13:26 +0900

    feat(CartService): CartService, 구현 완료

---
+++ src/main/java/christmas/service/CartService.java
@@ -0,0 +1,25 @@
+package christmas.service;
+
+import christmas.domain.Cart;
+import christmas.domain.Menu;
+import christmas.view.InputView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static christmas.service.MenuService.makeMenuMap;
+
+public class CartService {
+
+    public static Cart makeCart() {
+        Cart cart = new Cart();
+        List<String> menuInput = InputView.readMenu();
+        HashMap<String, Integer> menuMap = makeMenuMap(menuInput);
+
+        for (String menuName : menuMap.keySet()) {
+            Menu menu = Menu.getMenu(menuName);
+            cart.addMenuInCart(menu, menuMap.get(menuName));
+        }
+        return cart;
+    }
+}
