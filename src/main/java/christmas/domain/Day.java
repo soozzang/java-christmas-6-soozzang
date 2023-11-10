b1c13de54cc0a9c00d775dbce1a5dd78d49dd69c
soozzang <soozzang@inha.edu>
2023-11-10 15:15:23 +0900

    feat(Day): 요일 판별하는 메서드 생성

--- src/main/java/christmas/domain/Day.java
+++ src/main/java/christmas/domain/Day.java
@@ -1,9 +1,19 @@
 package christmas.domain;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+
 public enum Day {
     WEEKDAY,WEEKEND;
 
     public static Day daygenerator(int dateInput) {
-        return
+        int year = 2023;
+        Month month = Month.DECEMBER;
+        DayOfWeek dayOfWeek = LocalDate.of(dateInput,month,year).getDayOfWeek();
+        if (dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY) {
+            return WEEKEND;
+        }
+        return WEEKDAY;
     }
 }
