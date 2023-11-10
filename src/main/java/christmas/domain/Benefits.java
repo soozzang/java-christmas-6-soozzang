38436f5e9ad5af767280fd8c69ec5ef6123f6078
soozzang <soozzang@inha.edu>
2023-11-10 23:12:42 +0900

    refactor(Benefits): Benefits, 구현 완료

--- src/main/java/christmas/domain/Benefits.java
+++ src/main/java/christmas/domain/Benefits.java
@@ -1,5 +1,9 @@
 package christmas.domain;
 
+import christmas.service.BenefitsService;
+
+import java.text.NumberFormat;
+
 public class Benefits {
 
     private final long christmasDdayDiscount;
@@ -9,28 +13,91 @@ public class Benefits {
 
     public Benefits(Date date, Cart cart) {
         this.christmasDdayDiscount = calculateChristmasDdayDiscount(date);
-        this.dayDiscount = calculateDayDiscount(date);
-        this.starDisount = calculateStarDisount(date);
+        this.dayDiscount = calculateDayDiscount(date,cart);
+        this.starDisount = calculateStarDisount(date,cart);
         this.champagne = calculateChampagne(cart.calculatdTotalPrice());
+    }
+
+    public String getChampagneForPrint() {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        numberFormat.setGroupingUsed(true);
+        return numberFormat.format(champagne);
+    }
+
+    public String getChristmasDdayDiscountForPrint() {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        numberFormat.setGroupingUsed(true);
+        return numberFormat.format(christmasDdayDiscount);
+    }
+
+    public String getDayDiscountForPrint() {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        numberFormat.setGroupingUsed(true);
+        return numberFormat.format(dayDiscount);
+    }
+
+    public String getStarDisountForPrint() {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        numberFormat.setGroupingUsed(true);
+        return numberFormat.format(starDisount);
     }
 
     private long calculateChristmasDdayDiscount(Date date) {
         // date 검증함수 후 christmasDdayDiscount 설정
+        if (!date.getIsChristMasDayDiscount()) {
+            return 0;
+        }
+        // 계산로직
+        long christmansDdayDiscount = 1000;
+        christmansDdayDiscount += 100L*(date.getDate() - 1 );
+        return christmansDdayDiscount;
     }
 
-    private long calculateDayDiscount(Date date) {
+    private long calculateDayDiscount(Date date, Cart cart) {
         // date 검증함수 후 dayDiscount 설정
+        long dayDiscount = 0;
+        if (date.getDay() == Day.WEEKDAY) {
+            dayDiscount += BenefitsService.calculateDiscountInWeekday(cart);
+        }
+        if (date.getDay() == Day.WEEKEND) {
+            dayDiscount += BenefitsService.calculateDiscountInWeekend(cart);
+        }
+        return dayDiscount;
     }
 
-    private long calculateStarDisount(Date date) {
+    private long calculateStarDisount(Date date, Cart cart) {
         // date 검증함수 후 starDiscount 설정
+        if (!date.getIsStarDate()) {
+            return 0;
+        }
+        // 계산 로직
+        return 1000L;
     }
 
     private long calculateChampagne(long totalPrice) {
         // date 검증함수 후 Champagne 설정
+        if (totalPrice >= 120000) {
+            return 25000L;
+        }
+        return 0;
     }
 
     public long calculateTotalBenefits() {
-        return
+        long totalBenefits = 0;
+        totalBenefits += christmasDdayDiscount;
+        totalBenefits += dayDiscount;
+        totalBenefits += starDisount;
+        totalBenefits += champagne;
+        return totalBenefits;
+    }
+
+    public long calculateTotalDiscount() {
+        return calculateTotalBenefits() - champagne;
+    }
+
+    public String totalBenefitsForPrint() {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        numberFormat.setGroupingUsed(true);
+        return numberFormat.format(calculateTotalBenefits());
     }
 }
