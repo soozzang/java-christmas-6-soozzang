d839eeecd3a8efd47966a4e60e01889856ba6fda
soozzang <soozzang@inha.edu>
2023-11-10 14:59:03 +0900

    feat(BenefitsService): BenefitsService, 컨트롤러 기본 뼈대 설계

---
+++ src/main/java/christmas/service/BenefitsService.java
@@ -0,0 +1,17 @@
+package christmas.service;
+
+public class BenefitsService {
+
+    // 크리스마스 디데이 할인 검증함수
+    public
+
+    // 주말 / 평일 검증 함수
+    public
+
+    // 별 달린 날인지 검증 함수
+    public
+
+    // 샴페인 증정 가능한지 검증 함수
+    public
+
+}
