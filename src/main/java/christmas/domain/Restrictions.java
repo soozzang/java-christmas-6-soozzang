910bab8568758ed62d47dd8d7db2106bfdd7baa6
soozzang <soozzang@inha.edu>
2023-11-10 23:16:44 +0900

    feat(Restrictions): 추가 로직 구현완료

--- src/main/java/christmas/domain/Restrictions.java
+++ src/main/java/christmas/domain/Restrictions.java
@@ -3,25 +3,47 @@ package christmas.domain;
 public class Restrictions {
 
     private final boolean isOverTenthousand;
-    private final boolean isOnlyDrink;
+    private final boolean NotOnlyDrink;
     private final boolean isOverTwenty;
 
     public Restrictions(Cart cart) {
         this.isOverTenthousand = checkIsOverTenthousand(cart);
-        this.isOnlyDrink = checkIsOnlyDrink(cart);
+        this.NotOnlyDrink = checkIsNotOnlyDrink(cart);
         this.isOverTwenty = checkIsOverTwenty(cart);
     }
 
-    public boolean checkIsOverTenthousand(Cart cart) {
-        //
+    public boolean getIsOverTenthousand() {
+        return isOverTenthousand;
     }
 
-    public boolean checkIsOnlyDrink(Cart cart) {
-        //
+    public boolean getNotOnlyDrink() {
+        return NotOnlyDrink;
+    }
+
+    public boolean getIsOverTwenty() {
+        return isOverTwenty;
+    }
+
+    public boolean checkIsOverTenthousand(Cart cart) {
+        if (cart.calculatdTotalPrice() >= 10000L) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkIsNotOnlyDrink(Cart cart) {
+        boolean checkIsNotOnlyDrink = false;
+        for (Menu menu : cart.getCart().keySet()) {
+            if (!menu.getType().equals("음료")) {
+                checkIsNotOnlyDrink = true;
+                break;
+            }
+        }
+        return checkIsNotOnlyDrink;
     }
 
     public boolean checkIsOverTwenty(Cart cart) {
-        //
+        return cart.getCartSize() <= 20 && cart.getCartSize()>0;
     }
 
 }
