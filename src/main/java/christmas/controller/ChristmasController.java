af6bcf5659cfefa75c69dba1f554803f8e12d716
soozzang <soozzang@inha.edu>
2023-11-10 23:18:40 +0900

    feat(Controller): 컨트롤러 로직 구현 및 Application에 연결

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,7 +1,11 @@
 package christmas.controller;
 
 import christmas.domain.*;
+import christmas.service.CartService;
+import christmas.service.DateService;
+import christmas.service.RestrictionsService;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 import java.util.HashMap;
 import java.util.List;
@@ -9,51 +13,35 @@ import java.util.List;
 import static christmas.service.MenuService.*;
 
 public class ChristmasController {
-    Cart cart = new Cart();
-    Restrictions restrictions = new Restrictions(cart);
     public void start() {
-        // 날짜 입력
-        int dateInput = InputView.readDate();
-        // 무슨 요일인지 검증
-        Day day = Day.daygenerator(dateInput);
-        Date date = new Date(dateInput,day);
-
-        // 주문 메뉴 및 개수
-        List<String> menuInput = InputView.readMenu();
-
-        // 카트에 넣기
-        HashMap<String, Integer> menuMap = makeMenuMap(menuInput);
-        for (String menuName : menuMap.keySet()) {
-            Menu menu = Menu.getMenu(menuName);
-            cart.addMenuInCart(menu, menuMap.get(menuName));
-        }
-
-        // 제한 사항 검토
-        restrictions.checkIsOverTenthousand(cart);
-        // 추가 로직 (검토 사항에 따라 제한)
-
-        // 고객 도메인 채우기
+        Date date = DateService.makeDate();
+        Cart cart = CartService.makeCart();
         Customer customer = new Customer(cart.calculatdTotalPrice());
+        Restrictions restrictions = new Restrictions(cart);
 
-        // 주문 메뉴 출력
-        // 할인 전 총주문 금액 출력
-
-        // 혜택 도메인 채우기
-        Benefits benefits = new Benefits(date,cart);
-
-        // 샴페인 증정하는 지 검증 후 출력
-        // 메서드(benefits) boolean 뱉음
-
-        // 혜택 내역을 출력한다.
-
-        // 총 혜택 금액 출력
-        long totalBenefits = benefits.calculateTotalBenefits();
-
-        // 할인 후 예상 결제 금액 출력
-        long predictedPrice = customer.getPaidMoneyBeforeDiscount() - benefits.calculateTotalBenefits();
-
-        // 배지 선택 후 출력
-        customer.setCustomerBadge(totalBenefits);
-        customer.getBadge();
+        if (RestrictionsService.checkIsAbleToOrder(restrictions)){
+            if (!RestrictionsService.checkIsAbleToApplyEvent(restrictions)) {
+                startIfIsUnableToGetBenefits(customer);
+                return;
+            }
+            startIfIsAbleToOrder(cart, date, customer);
+            return;
+        }
+        System.out.println("구매 불가");
     }
+
+    public void startIfIsAbleToOrder(Cart cart, Date date, Customer customer) {
+        OutputView.printAboutMenu(cart);
+        Benefits benefits = new Benefits(date,cart);
+        customer.setCustomerBadge(benefits.calculateTotalBenefits());
+        long predictedPrice = customer.getPaidMoneyBeforeDiscount() - benefits.calculateTotalDiscount();
+        OutputView.printAfterChampagne(benefits,predictedPrice,customer.getBadge());
+    }
+
+    public void startIfIsUnableToGetBenefits(Customer customer) {
+        OutputView.printNoBenefits(customer.getPaidMoneyBeforeDiscount());
+    }
+
+
+
 }
