9c78966877df722a8c91c16ed7744c07ecb04332
soozzang <soozzang@inha.edu>
2023-11-10 14:59:29 +0900

    feat(InputView): InputView, 컨트롤러 기본 뼈대 설계

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,18 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class InputView {
+    public static int readDate() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+    }
+
+    public static List<String> readMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+    }
+}
