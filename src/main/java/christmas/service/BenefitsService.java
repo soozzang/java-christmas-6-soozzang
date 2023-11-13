e4c27ca09b7ca31adb04c0b05687dd28a19f48a5
soozzang <soozzang@inha.edu>
2023-11-13 15:35:11 +0900

    refactor(BenefitsService): 평일/주말할인 로직 수정

--- src/main/java/christmas/service/BenefitsService.java
+++ src/main/java/christmas/service/BenefitsService.java
@@ -7,20 +7,22 @@ import christmas.domain.Menu;
 public class BenefitsService {
 
     public static long calculateDiscountInWeekday(Cart cart) {
+        int desertCount = 0;
         for (Menu menu : cart.getCart().keySet()) {
             if (menu.getType().equals("디저트")) {
-                return 2023L * (cart.getCart().get(menu));
+                desertCount += (cart.getCart().get(menu));
             }
         }
-        return 0;
+        return 2023L * desertCount;
     }
 
     public static long calculateDiscountInWeekend(Cart cart) {
+        int mainCount = 0;
         for (Menu menu : cart.getCart().keySet()) {
             if (menu.getType().equals("메인")) {
-                return 2023L * (cart.getCart().get(menu));
+                mainCount += (cart.getCart().get(menu));
             }
         }
-        return 0;
+        return 2023L * mainCount;
     }
 }
